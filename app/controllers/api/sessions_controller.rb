31a84e2b9a23351a9237451c3a28c95eeac2a49b
Gabriel Talavera <gabrielvt6@gmail.com>
2018-04-03 14:39:24 -0700

    Complete Session and User controllers

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,25 @@
+class Api::SessionsController < ApplicationController
+  def create
+    debugger
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    );
+    if (@user)
+      login(@user)
+      render 'api/users/show'
+    else
+      render json: @user.errors.full_messages, status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      signout
+      render 'api/users/show'
+    else
+      render json: ['Not signed in'], status: 404
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :show]
-    resource :session, only: [:create, :destroy, :show]
+    resource :session, only: [:create, :destroy]
   end
 
   root "static_pages#root"
