7d043bf930e0a6baf77c6ca65924efd4c1d8c5da
Gabriel Talavera <gabrielvt6@gmail.com>
2018-04-04 10:56:58 -0700

    Allow user to stay logged in after refreshing page

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -7,8 +7,9 @@ import Root from './components/root';
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   let store;
+
   if (window.currentUser) {
-    const preloadedState = {session: { currentUser: window.CurrentUser } };
+    const preloadedState = {session: { currentUser: window.currentUser } };
     store = configureStore(preloadedState);
     delete window.currentUser;
   } else {
@@ -19,7 +20,9 @@ document.addEventListener('DOMContentLoaded', () => {
   window.signup = signup;
   window.login = login;
   window.logout = logout;
-  window.dispatch = store.dispatch;
+
   // END TESTING
+  window.dispatch = store.dispatch;
+
   ReactDOM.render(<Root store={ store }/>, root);
 });
