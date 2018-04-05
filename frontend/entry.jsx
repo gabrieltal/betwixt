bbe9aa06fc9469ed3e530da1f73c84a2568d6289
Gabriel Talavera <gabrielvt6@gmail.com>
2018-04-05 08:11:24 -0700

    minor edits

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -6,7 +6,6 @@ import Root from './components/root';
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   let store;
-
   if (window.currentUser) {
     const preloadedState = {session: { currentUser: window.currentUser } };
     store = configureStore(preloadedState);
