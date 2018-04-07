114777522c4b047d7ecfbfe0747232d03246f40c
Gabriel Talavera <gabrielvt6@gmail.com>
2018-04-07 13:36:57 -0700

    Users authored stories now display on user show page

--- frontend/components/user/user_show.jsx
+++ frontend/components/user/user_show.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
-
+import StoryIndexItem from '../story/story_index_item';
 class UserShow extends React.Component{
   constructor (props) {
     super(props);
   }
   componentDidMount() {
-    this.props.fetchUser(this.props.userId)
+    this.props.fetchUser(this.props.userId);
+    this.props.fetchStories();
   }
   render () {
-    if (!!this.props.user) {
+    if (!!this.props.user || !!this.props.authoredStories) {
       const user = this.props.user;
+      let stories = this.props.authoredStories;
+      stories = Object.keys(stories).map((id) => {
+        return <StoryIndexItem key={id} story={stories[id]}/>;
+      });
       return (
         <div className="user-show">
           <h3>{user.username}</h3>
           <p className="bio">{user.bio}</p>
           <p className="member-creation">Betwixt member since {user.created_at}</p>
           <img className="user-profile-pic" src={user.image_url}/>
+          <ul>
+            {stories}
+          </ul>
         </div>
       );
     } else {
--- frontend/components/user/user_show_container.js
+++ frontend/components/user/user_show_container.js
@@ -1,16 +1,20 @@
 import { connect } from 'react-redux';
 import UserShow from './user_show';
 import { fetchUser } from '../../actions/user_actions';
+import { fetchStories } from '../../actions/story_actions';
 import { selectAuthoredStories } from '../../reducers/selector'
 const mapStateToProps = (state, ownProps) => {
+  const user = state.users[ownProps.match.params.userId];
   return {
-    user: state.users[ownProps.match.params.userId],
+    user,
+    authoredStories: selectAuthoredStories(state, user),
     userId: ownProps.match.params.userId
   }
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchUser: (id) => dispatch(fetchUser(id))
+  fetchUser: (id) => dispatch(fetchUser(id)),
+  fetchStories: () => dispatch(fetchStories())
 });
 
 export default connect(
