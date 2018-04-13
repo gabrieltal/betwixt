83355b3baafea5a1bcbda945d1d037e0c4d8f1e5
Gabriel Talavera <gabrielvt6@gmail.com>
2018-04-12 18:50:39 -0700

    Create follow model

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,10 @@
 class Follow < ApplicationRecord
+  validates :follower_id, uniqueness: { scope: :following_id }
+  has_many :followers,
+    class_name: :User,
+    foreign_key: :follower_id
+
+  has_many :following,
+    class_name: :User,
+    foreign_key: :following_id
 end
